f9dcf28eda5dbe916d2851c3b2d4f78a80004ef1
Hadassah Uche <uhadassah1@gmail.com>
2023-11-02 16:12:54 +0200

    Implemented the loop to read the 10 positive numbers into the array and a while loop to check if a negative number is entered

--- UcheHadassah3_1a/HadassahUche3_1a.cpp
+++ UcheHadassah3_1a/HadassahUche3_1a.cpp
@@ -9,6 +9,17 @@ int main()
 	int Copy[10];
 	int num;
 	cout << "Enter 10 positive numbers:";
-	
+	for (int i = 0; i < 10; i++)
+	{
+		int num;
+		cin >> num;
+		while (num < 0)
+		{
+			cout << "Invalid! Please enter a POSITIVE number:";
+				cin >> num;
+		}
+		Original[i] = num;
+		Copy[i] = Original[i];
+	}
 	return 0;
 }
